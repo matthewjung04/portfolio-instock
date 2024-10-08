823d5235e563a3939837fb7fdaf3130b1dacd997
Talha Pervaiz <167258243+MTalha-Pervaiz@users.noreply.github.com>
2024-10-08 13:28:43 -0400

    Ticket A24PT-3 fixed and completed.

---
+++ src/components/AddInventory.jsx
@@ -0,0 +1,10 @@
+function AddInventory() {
+  return (
+    <div>
+      <h1>Add New Inventory Item</h1>
+      <p>Form to add a new inventory item will go here.</p>
+    </div>
+  );
+}
+
+export default AddInventory;
---
+++ src/components/Warehouses.jsx
@@ -0,0 +1,10 @@
+function Warehouses() {
+  return (
+    <div>
+      <h1>Warehouses</h1>
+      <p>This is the warehouses page.</p>
+    </div>
+  );
+}
+
+export default Warehouses;
