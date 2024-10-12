aee0523df671a6a2e5ea52c3a74004c94907b249
JuanDavidDV <juandadazav@gmail.com>
2024-10-11 21:53:26 -0400

    warehouse details header completed for mobile, new mixin added for page layout

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -9,9 +9,11 @@ const WarehouseDetails = () => {
     <section className="warehouse-details">
         <div className="warehouse-details__container">
             <div className="warehouse-details__container__top">
-                <img className="warehouse-details__container__top--arrow" src={arrowBack}/>
-                <h1 className="warehouse-details__container__top--title" >Washington</h1>
-                <img className="warehouse-details__container__top--edit" src={editWhite}/>
+                <div className="warehouse-details__container__top__box">
+                    <img className="warehouse-details__container__top__box--arrow" src={arrowBack}/>
+                    <h1 className="warehouse-details__container__top__box--title" >Washington</h1>
+                </div>
+                    <img className="warehouse-details__container__top--edit" src={editWhite}/>
             </div>
       </div>
 
