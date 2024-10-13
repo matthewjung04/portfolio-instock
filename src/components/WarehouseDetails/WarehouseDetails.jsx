351303560cbcc6ef52d4164763f2f4386dc3bbea
JuanDavidDV <juandadazav@gmail.com>
2024-10-12 21:04:50 -0400

    instock tag mixin created

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -52,30 +52,35 @@ const WarehouseDetails = () => {
         </div>
         <div className="warehouse-details__wrapper-inventory">
             <div className="warehouse-details__wrapper-inventory__container">
-                <div className="warehouse-details__wrapper-inventory__container--one">
-                    <div className="warehouse-details__wrapper-inventory__container--one--item">
-                        <h4>INVENTORY ITEM</h4>
-                        <div className="warehouse-details__wrapper-inventory__container--one--item--box">
-                            <h3>Television</h3>
-                            <img src={chevron}/>
-                        </div>     
+                <div className="warehouse-details__wrapper-inventory__container__top">
+                    <div className="warehouse-details__wrapper-inventory__container__top--one">
+                        <div className="warehouse-details__wrapper-inventory__container__top--one--item">
+                            <h4>INVENTORY ITEM</h4>
+                            <div className="warehouse-details__wrapper-inventory__container__top--one--item--box">
+                                <h3 className="warehouse-details__wrapper-inventory__container__top--one--item--box--name">Television</h3>
+                                <img src={chevron}/>
+                            </div>     
+                        </div>
+                        <div className="warehouse-details__wrapper-inventory__container__top--one--category">
+                            <h4>CATEGORY</h4>
+                            <p className="warehouse-details__wrapper-inventory__container__top--one--category--text p2">Electronics</p>
+                        </div>
                     </div>
-                    <div className="warehouse-details__wrapper-inventory__container--one--category">
-                        <h4>CATEGORY</h4>
-                        <p className="p2">Electronics</p>
+                    <div className="warehouse-details__wrapper-inventory__container__top--two">
+                        <div className="warehouse-details__wrapper-inventory__container__top--two--status">
+                            <h4>STATUS</h4>
+                            <div className="warehouse-details__wrapper-inventory__container__top--two--status--box">
+                                <h4 className="warehouse-details__wrapper-inventory__container__top--two--status--box--text">IN STOCK</h4>
+                            </div>
+                                
+                        </div>
+                        <div className="warehouse-details__wrapper-inventory__container__top--two--quantity">
+                            <h4>QTY</h4>
+                            <p className="p2">500</p>
+                        </div>
                     </div>
                 </div>
-                <div className="warehouse-details__wrapper-inventory__container--two">
-                    <div className="warehouse-details__wrapper-inventory__container--two--status">
-                        <h4>STATUS</h4>
-                        <h4>IN STOCK</h4>
-                    </div>
-                    <div className="warehouse-details__wrapper-inventory__container--two--quantity">
-                        <h4>QTY</h4>
-                        <p className="p2">500</p>
-                    </div>
-                </div>
-                <div className="warehouse-details__wrapper-inventory__container--three">
+                <div className="warehouse-details__wrapper-inventory__container__bottom">
                     <img src={deleteIcon}/>
                     <img src={editBlue}/>
                 </div>
