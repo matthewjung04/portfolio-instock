7b5b12dd93071ea4f85a3c3605879686c59103ee
JuanDavidDV <juandadazav@gmail.com>
2024-10-13 23:13:49 -0400

    front end API axios request completed

--- src/components/WarehouseDetails/WarehouseDetails.jsx
+++ src/components/WarehouseDetails/WarehouseDetails.jsx
@@ -1,4 +1,3 @@
-import { useParams } from 'react-router-dom';
 import arrowBack from "../../assets/icons/arrow_back-24px.svg";
 import editWhite from "../../assets/icons/edit-white-24px.svg";
 import chevron from "../../assets/icons/chevron_right-24px.svg";
@@ -6,88 +5,88 @@ import editBlue from "../../assets/icons/edit-24px.svg";
 import deleteIcon from "../../assets/icons/delete_outline-24px.svg"
 import './WarehouseDetails.scss'
 
-const WarehouseDetails = () => {
-  const { id } = useParams();
-  return (
-    <section className="warehouse-details">
-        <div className="warehouse-details__container">
-            <div className="warehouse-details__container__top">
-                <div className="warehouse-details__container__top__box">
-                    <img className="warehouse-details__container__top__box--arrow" src={arrowBack}/>
-                    <h1 className="warehouse-details__container__top__box--title" >Washington</h1>
-                </div>
-                    <img className="warehouse-details__container__top--edit" src={editWhite}/>
-            </div> 
-        </div>
-        <div className="warehouse-details__wrapper">
-            <div className="warehouse-details__wrapper__container">
-                <div className="warehouse-details__wrapper__container__address">
-                    <h4>WAREHOUSE ADDRESS:</h4>
-                    <div className="warehouse-details__wrapper__container__address__text">
-                    <p className="warehouse-details__wrapper__container__address--text--one p2">
-                            33 Pearl Street SW, 
-                        </p>
-                        <p className="warehouse-details__wrapper__container__address--text--two p2"> 
-                            Washington, USA
-                        </p>
-                    </div>        
-                </div>                  
-                <div className="warehouse-details__wrapper__container__bottom">
-                    <div className="warehouse-details__wrapper__container__bottom--name">
-                        <h4>CONTACT NAME:</h4>
-                        <p className="warehouse-details__wrapper__container__bottom--name--text p2">
-                            Graeme Lyon 
-                            <br/>Warehouse Manager
-                        </p>
+const WarehouseDetails = ({ warehouseName, warehouseAddress, warehouseCity, warehouseCountry, warehouseContactName, warehouseContactPosition, warehouseContactPhone, warehouseContactEmail }) => {
+    
+    return (
+        <section className="warehouse-details">
+            <div className="warehouse-details__container">
+                <div className="warehouse-details__container__top">
+                    <div className="warehouse-details__container__top__box">
+                        <img className="warehouse-details__container__top__box--arrow" src={arrowBack}/>
+                        <h1 className="warehouse-details__container__top__box--title">{warehouseName}</h1>
                     </div>
-                    <div className="warehouse-details__wrapper__container__bottom--information">
-                        <h4>CONTACT INFORMATION:</h4>
-                        <p className="warehouse-details__wrapper__container__bottom--information--text p2">
-                            +1 (647) 504-0911
-                            <br/>gylon@instock.com
-                        </p>
+                        <img className="warehouse-details__container__top--edit" src={editWhite}/>
+                </div> 
+            </div>
+            <div className="warehouse-details__wrapper">
+                <div className="warehouse-details__wrapper__container">
+                    <div className="warehouse-details__wrapper__container__address">
+                        <h4>WAREHOUSE ADDRESS:</h4>
+                        <div className="warehouse-details__wrapper__container__address__text">
+                        <p className="warehouse-details__wrapper__container__address--text--one p2">
+                                {warehouseAddress}, 
+                            </p>
+                            <p className="warehouse-details__wrapper__container__address--text--two p2"> 
+                                {warehouseCity}, {warehouseCountry}
+                            </p>
+                        </div>        
+                    </div>                  
+                    <div className="warehouse-details__wrapper__container__bottom">
+                        <div className="warehouse-details__wrapper__container__bottom--name">
+                            <h4>CONTACT NAME:</h4>
+                            <p className="warehouse-details__wrapper__container__bottom--name--text p2">
+                                {warehouseContactName} 
+                                <br/>{warehouseContactPosition}
+                            </p>
+                        </div>
+                        <div className="warehouse-details__wrapper__container__bottom--information">
+                            <h4>CONTACT INFORMATION:</h4>
+                            <p className="warehouse-details__wrapper__container__bottom--information--text p2">
+                                {warehouseContactPhone}
+                                <br/>{warehouseContactEmail}
+                            </p>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div className="warehouse-details__wrapper-inventory">
-            <div className="warehouse-details__wrapper-inventory__container">
-                <div className="warehouse-details__wrapper-inventory__container__top">
-                    <div className="warehouse-details__wrapper-inventory__container__top--one">
-                        <div className="warehouse-details__wrapper-inventory__container__top--one--item">
-                            <h4>INVENTORY ITEM</h4>
-                            <div className="warehouse-details__wrapper-inventory__container__top--one--item--box">
-                                <h3 className="warehouse-details__wrapper-inventory__container__top--one--item--box--name">Television</h3>
-                                <img src={chevron}/>
-                            </div>     
-                        </div>
-                        <div className="warehouse-details__wrapper-inventory__container__top--one--category">
-                            <h4>CATEGORY</h4>
-                            <p className="warehouse-details__wrapper-inventory__container__top--one--category--text p2">Electronics</p>
-                        </div>
-                    </div>
-                    <div className="warehouse-details__wrapper-inventory__container__top--two">
-                        <div className="warehouse-details__wrapper-inventory__container__top--two--status">
-                            <h4>STATUS</h4>
-                            <div className="warehouse-details__wrapper-inventory__container__top--two--status--box">
-                                <h4 className="warehouse-details__wrapper-inventory__container__top--two--status--box--text">IN STOCK</h4>
+            <div className="warehouse-details__wrapper-inventory">
+                <div className="warehouse-details__wrapper-inventory__container">
+                    <div className="warehouse-details__wrapper-inventory__container__top">
+                        <div className="warehouse-details__wrapper-inventory__container__top--one">
+                            <div className="warehouse-details__wrapper-inventory__container__top--one--item">
+                                <h4>INVENTORY ITEM</h4>
+                                <div className="warehouse-details__wrapper-inventory__container__top--one--item--box">
+                                    <h3 className="warehouse-details__wrapper-inventory__container__top--one--item--box--name">Television</h3>
+                                    <img src={chevron}/>
+                                </div>     
                             </div>
-                                
+                            <div className="warehouse-details__wrapper-inventory__container__top--one--category">
+                                <h4>CATEGORY</h4>
+                                <p className="warehouse-details__wrapper-inventory__container__top--one--category--text p2">Electronics</p>
+                            </div>
                         </div>
-                        <div className="warehouse-details__wrapper-inventory__container__top--two--quantity">
-                            <h4>QTY</h4>
-                            <p className="warehouse-details__wrapper-inventory__container__top--two--quantity--number p2">500</p>
+                        <div className="warehouse-details__wrapper-inventory__container__top--two">
+                            <div className="warehouse-details__wrapper-inventory__container__top--two--status">
+                                <h4>STATUS</h4>
+                                <div className="warehouse-details__wrapper-inventory__container__top--two--status--box">
+                                    <h4 className="warehouse-details__wrapper-inventory__container__top--two--status--box--text">IN STOCK</h4>
+                                </div>
+                                    
+                            </div>
+                            <div className="warehouse-details__wrapper-inventory__container__top--two--quantity">
+                                <h4>QTY</h4>
+                                <p className="warehouse-details__wrapper-inventory__container__top--two--quantity--number p2">500</p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="warehouse-details__wrapper-inventory__container__bottom">
-                    <img src={deleteIcon}/>
-                    <img src={editBlue}/>
+                    <div className="warehouse-details__wrapper-inventory__container__bottom">
+                        <img src={deleteIcon}/>
+                        <img src={editBlue}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    </section>
-  );
+        </section>
+    );
 }
 
 export default WarehouseDetails;
--- src/pages/WarehousePage/WarehousePage.jsx
+++ src/pages/WarehousePage/WarehousePage.jsx
@@ -1,10 +1,46 @@
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from "axios";
 import './WarehousePage.scss'
 import WarehouseDetails from '../../components/WarehouseDetails/WarehouseDetails';
 
-const WarehousePage =() => {
+const WarehousePage = () => {
+
+    const { id } = useParams();
+    const [warehouseDetails, setWarehouseDetails] = useState(null);
+
+    const baseUrl = import.meta.env.VITE_API_URL;
+
+    const fetchWarehouseDetails = async (warehouseSelectedId) => {
+        try {
+            const { data } = await axios.get(baseUrl + "/warehouses/" + warehouseSelectedId);
+            setWarehouseDetails(data);
+            console.log(data);
+            return setWarehouseDetails;
+        }
+        catch(error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        fetchWarehouseDetails(id);
+    }, [id]);
+
+
     return (
         <>
-            <WarehouseDetails />
+            { warehouseDetails ? <WarehouseDetails 
+                warehouseName = {warehouseDetails.warehouse_name}
+                warehouseAddress = {warehouseDetails.address}
+                warehouseCity = {warehouseDetails.city}
+                warehouseCountry = {warehouseDetails.country}
+                warehouseContactName = {warehouseDetails.contact_name}
+                warehouseContactPosition = {warehouseDetails.contact_position}
+                warehouseContactPhone = {warehouseDetails.contact_phone}
+                warehouseContactEmail = {warehouseDetails.contact_email}
+            /> : "loading"
+            }
         </>
     )
 }
