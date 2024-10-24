7fa32ebef455f5d4087c9e1217b443b557676606
JuanDavidDV <juandadazav@gmail.com>
2024-10-24 19:59:19 -0400

    folders created to delete inventory feature

---
+++ src/components/DeleteInventory/DeleteInventory.jsx
@@ -0,0 +1,13 @@
+
+
+const DeleteInventory = () => {
+
+    const baseUrl = import.meta.env.VITE_API_URL || 'http://localhost:8080';
+    return (
+        <div className="">
+
+        </div>
+    )
+}
+
+export default DeleteInventory;
